65ab14565912c398f1a8964df178dd9dc9fa0d74
rgovindaraj6092@gmail.com <rgovindaraj6092@gmail.com>
2022-11-06 18:37:12 +0530

    button added,api response

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let advice_area = document.getElementById('advice');
-
+let number= document.getElementById('num');
 async function getAdvice(){
 
 	let request = new Request("https://api.adviceslip.com/advice");
 	let responce = await fetch(request);
 	let advice = await responce.json();
-	advice.forEach(a=>alert(a));
-	//alert(advice)
+	
+  console.log(advice.slip.advice)
+  advice_area.innerText = advice.slip.advice;
+  number.innerText=advice.slip.id;
 }
 
 getAdvice();
